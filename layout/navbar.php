1798c8ee8e46d03fbe10ebc234ab8fdde4fd3e7a
sisil <sisilia@google.com>
2024-11-08 22:52:34 +0700

    mengaktifkan dropdown navbar :)

--- layout/navbar.php
+++ layout/navbar.php
@@ -7,7 +7,7 @@
         <div class="group"> -->
           <div class="navbar">
             <div class="navbar-2">
-              <nav class="navbar navbar-expand-lg" style="background-color: (31, 43, 108, 1);">
+              <nav class="navbar navbar-expand-lg" style="background-color: rgba(31, 43, 108, 1);">
                 <div class="container-fluid">
                     <button class="navbar-toggler navbar-dark" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
@@ -21,22 +21,22 @@
                                 <a class="nav-link" href="profile.php">Profile</a>
                             </li>
                             <li class="nav-item dropdown text-wrapper-23">
-                                <a class="nav-link dropdown-toggle" href="layanan.php" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">Layanan</a>
+                                <a class="nav-link dropdown-toggle" href="layanan.php" id="navbarDropdown1" role="button" data-bs-toggle="dropdown" aria-expanded="false">Layanan</a>
                                 <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
                                 <li><a class="dropdown-item" href="layanan.php">Dokter</a></li>
                                 <li><a class="dropdown-item" href="layanan.php">Layanan Medis</a></li>
-                                <li><a class="dropdown-item" href="#">Layanan Penunjang Medis</a></li>
+                                <li><a class="dropdown-item" href="layanan.php">Layanan Penunjang Medis</a></li>
                                 </ul>
                             </li>
                             <li class="nav-item dropdown text-wrapper-24">
-                                <a class="nav-link dropdown-toggle" href="fasilitas.php" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">Fasilitas</a>
+                                <a class="nav-link dropdown-toggle" href="fasilitas.php" id="navbarDropdown2" role="button" data-bs-toggle="dropdown" aria-expanded="false">Fasilitas</a>
                                 <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
                                 <li><a class="dropdown-item" href="fasilitas.php">Fasilitas Rawat Inap</a></li>
                                 <li><a class="dropdown-item" href="fasilitas.php">Fasilitas Publik</a></li>
                                 </ul>
                             </li>
                             <li class="nav-item text-wrapper-24">
-                                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">Informasi</a>
+                                <a class="nav-link dropdown-toggle" href="informasi.php" id="navbarDropdown3" role="button" data-bs-toggle="dropdown" aria-expanded="false">Informasi</a>
                                 <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
                                 <li><a class="dropdown-item" href="#">Jadwal Pelayanan</a></li>
                                 <li><a class="dropdown-item" href="#">Antrian Farmasi</a></li>
@@ -58,11 +58,11 @@
                 </div>
               </nav>
             </div>
-        </div>
+          </div>
       <!-- </div>
     </div> -->
   </body>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" 
-  integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" 
-  crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-QB6ZQq6NqGh9FTjVAJkB6NXOGmOS5VNafF9Vj/GNGa+vG2FwTfcb46f7AMzJ3Kzg" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cmRBT6Az3f5a2PN9p6IhbCpGiQipFPz5FQOaOUzgpeQ94Cdm3Bx+NC6P+YLTsZHB" crossorigin="anonymous"></script>
+
 </html>
