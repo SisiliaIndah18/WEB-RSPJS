8bc8541234a93001586e59151bbf04d49f206b9b
sisil <sisilia@google.com>
2024-11-14 01:26:28 +0700

    footer sudah responsif, tapi konten beranda malah ketutupan azuewww

--- layout/footer.php
+++ layout/footer.php
@@ -2,43 +2,43 @@
 <html>
   <body>
     <!-- <div class="beranda"> -->
-      <footer class="footer text-white pt-5" style="background-color: rgba(31, 43, 108, 1);">
+      <footer class="footer text-white pt-5">
         <div class="container">
-            <div class="row">
-                <div class="col-12 col-md-4 text-left mb-3">
-                  <div class="group-2">
-                    <img class="RSUD-PJS" src="img/rsud-pjs.svg" />
-                    <p class="memimpin-dalam">Memimpin dalam Keunggulan<br/>Medis, Perawatan Terpercaya</p>
-                  </div>
-                </div>
-                <div class="col-12 col-md-4 text-left mb-3">
-                  <div class="group-3">
-                    <div class="text-wrapper">Tentang Kami</div>
-                    <div class="group-4">
-                      <a href="layanan.html" role="button"><div class="text-wrapper-2">Dokter</div></a>
-                      <a href="layanan.html" role="button"><div class="text-wrapper-3">Indikator Mutu RS</div></a>
-                      <a href="layanan.html" role="button"><div class="text-wrapper-4">Standar Pelayanan</div></a>
-                      <a href="layanan.html" role="button"><div class="text-wrapper-5">Maklumat Pelayanan</div></a>
-                      <a href="layanan.html" role="button"><div class="text-wrapper-6">Jadwal Pelayanan</div></a> 
-                    </div>
-                  </div>
-                </div>
-                <div class="col-12 col-md-4 text-left mb-3">
-                  <div class="group-5">
-                    <div class="text-wrapper-7">Hubungi Kami</div>
-                    <div class="group-6">
-                      <div class="text-wrapper-8">Call Center : 082259776756</div>
-                      <p class="p">Gawat Darurat : (0518) 21118</p>
-                      <div class="text-wrapper-9">Email:  rsud.kotabaru@yahoo.co.id</div>
-                      <p class="jalan-jl-h-hasan">
-                        Jalan: Jl. H Hasan Basri No. 57, <br />Kabupaten Kota Baru, <br />Kalimantan Selatan 72113
-                      </p>
-                    </div>
-                    </div>
-                </div>
+            <!-- <div class="row"> -->
+                <!-- <div class="col-12 col-md-4 text-left mb-3"> -->
+            <div class="group-2">
+              <img class="RSUD-PJS" src="img/rsud-pjs.svg" />
+              <p class="memimpin-dalam">Memimpin dalam Keunggulan<br/>Medis, Perawatan Terpercaya</p>
             </div>
-            <img class="vector" src="img/vector-1.svg" />
-            <img class="vector-3" src="img/vector-3.svg" />
+            <!-- </div>
+            <div class="col-12 col-md-4 text-left mb-3"> -->
+            <div class="group-3">
+              <div class="text-wrapper">Tentang Kami</div>
+              <div class="group-4">
+                <a href="layanan.html" role="button"><div class="text-wrapper-2">Dokter</div></a>
+                <a href="layanan.html" role="button"><div class="text-wrapper-3">Indikator Mutu RS</div></a>
+                <a href="layanan.html" role="button"><div class="text-wrapper-4">Standar Pelayanan</div></a>
+                <a href="layanan.html" role="button"><div class="text-wrapper-5">Maklumat Pelayanan</div></a>
+                <a href="layanan.html" role="button"><div class="text-wrapper-6">Jadwal Pelayanan</div></a> 
+              </div>
+            </div>
+            <!-- </div>
+            <div class="col-12 col-md-4 text-left mb-3"> -->
+            <div class="group-5">
+              <div class="text-wrapper-7">Hubungi Kami</div>
+              <div class="group-6">
+                <div class="text-wrapper-8">Call Center : 082259776756</div>
+                <p class="p">Gawat Darurat : (0518) 21118</p>
+                <div class="text-wrapper-9">Email:  rsud.kotabaru@yahoo.co.id</div>
+                <p class="jalan-jl-h-hasan">
+                  Jalan: Jl. H Hasan Basri No. 57, <br />Kabupaten Kota Baru, <br />Kalimantan Selatan 72113
+                </p>
+              </div>
+            </div>
+                <!-- </div> -->
+            <!-- </div> -->
+            <img class="vector" src="img/vector-1.svg" alt="Ikon dekoratif 1"/>
+            <img class="vector-3" src="img/vector-3.svg" alt="Ikon dekoratif 2"/>
             <p class="text-wrapper-10 text-left">© 2024 RSUD Pangeran Jaya Sumitra All Rights Reserved by IT Support</p>
         </div>
       </footer>
